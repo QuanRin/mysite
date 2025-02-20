3c80b048e2a2b3722a69c6ad759f2c58b7a392e5
minhquan <nguyenquan28322@gmail.com>
2025-02-20 10:13:28 +0700

    Django Tutorial Part 5: Creating our home page

--- locallibrary/catalog/constants.py
+++ locallibrary/catalog/constants.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 GENRE_NAME_MAX_LENGTH = 200
 
 BOOK_TITLE_MAX_LENGTH = 200
@@ -6,12 +8,27 @@ BOOK_ISBN_MAX_LENGTH = 200
 
 BOOK_INSTANCE_IMPRINT_MAX_LENGTH = 200
 BOOK_INSTANCE_STATUS_MAX_LENGTH = 1
-BOOK_INSTANCE_LOAN_STATUS = (
-    ('m', 'Maintenance'),
-    ('o', 'On loan'),
-    ('a', 'Available'),
-    ('r', 'Reserved'),
-)
+
+class LoanStatus(Enum):
+    MAINTENANCE = 'm'
+    ON_LOAN = 'o'
+    AVAILABLE = 'a'
+    RESERVED = 'r'
+
+    @classmethod
+    def choices(cls):
+        """Return a tuple of (value, label) pairs for use in Django models."""
+        return tuple((status.value, status.name.capitalize()) for status in cls)
+
+    @classmethod
+    def get_label(cls, value):
+        """Get the human-readable label for a given value."""
+        for status in cls:
+            if status.value == value:
+                return status.name.capitalize()
+        return None
+
+BOOK_INSTANCE_LOAN_STATUS = LoanStatus.choices()
 
 AUTHOR_FIRST_NAME_MAX_LENGTH = 100
 AUTHOR_LAST_NAME_MAX_LENGTH = 100
